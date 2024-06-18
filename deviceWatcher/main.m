cc1ca2446d10075ef669eadb68f378e983d31ba6
Danil Korotenko <danil.korotenko@gmail.com>
2024-06-18 12:39:43 +0300

    Added target

---
+++ deviceWatcher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  deviceWatcher
+//
+//  Created by Danil Korotenko on 6/18/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
