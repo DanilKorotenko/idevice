5d48f1cbcbb8cce674628f7d496433ef4e09b8c6
Danil Korotenko <danil.korotenko@gmail.com>
2024-07-05 12:37:02 +0300

    UsbMuxPacket added.

---
+++ usbmuxUtils/UsbMuxPacket/UsbMuxPacket.h
@@ -0,0 +1,16 @@
+//
+//  UsbMuxPacket.h
+//  usbmuxDeviceInfo
+//
+//  Created by Danil Korotenko on 7/5/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UsbMuxPacket : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ usbmuxUtils/UsbMuxPacket/UsbMuxPacket.m
@@ -0,0 +1,12 @@
+//
+//  UsbMuxPacket.m
+//  usbmuxDeviceInfo
+//
+//  Created by Danil Korotenko on 7/5/24.
+//
+
+#import "UsbMuxPacket.h"
+
+@implementation UsbMuxPacket
+
+@end
