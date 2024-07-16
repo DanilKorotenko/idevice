e15163aa7c46a8ac4fad5d9f4a82cbb39772a97f
Danil Korotenko <danil.korotenko@gmail.com>
2024-07-16 12:35:18 +0300

    Added device message class.

---
+++ usbmuxUtils/UMDeviceMessage.h
@@ -0,0 +1,16 @@
+//
+//  UMDeviceMessage.h
+//  usbmuxDeviceInfo
+//
+//  Created by Danil Korotenko on 7/16/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UMDeviceMessage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ usbmuxUtils/UMDeviceMessage.m
@@ -0,0 +1,13 @@
+//
+//  UMDeviceMessage.m
+//  usbmuxDeviceInfo
+//
+//  Created by Danil Korotenko on 7/16/24.
+//
+
+#import "UMDeviceMessage.h"
+
+// This is adapter to mutable dictionary
+@implementation UMDeviceMessage
+
+@end
